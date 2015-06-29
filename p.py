3b344fc48a3f214f8044a997c5184bbe4651e7ab
Athira14 <athirabalachandran333@gmail.com>
2015-06-29 16:18:58 +0530

    branch commit

--- p.py
+++ p.py
@@ -1,1 +1,12 @@
 print "hello"
+string=input("enter the string")
+count={}
+for char in string:
+ if char in count.keys():
+    count[char] +=1
+ else:
+    count[char] =1
+for i in count:   
+  
+ print ('%s' %(i))
+ print ('*\n'*count[i],)
